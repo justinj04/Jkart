f5951de21e23bcbbd28f6239c75288c33fa74db1
justinj04 <justinjoseph1104@gmail.com>
2025-02-05 21:39:39 +0530

    project name changed

--- Bazario/asgi.py
+++ Bazario/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Jkart project.
+ASGI config for Bazario project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jkart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bazario.settings')
 
 application = get_asgi_application()
